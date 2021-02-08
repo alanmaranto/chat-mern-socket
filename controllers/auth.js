ed1b6968e346eb4f12dab644a9abae8f5eae5376
alan <alanarostegui@hotmail.com>
2021-02-08 13:30:41 -0600

    refactor(auth): add middleware to validate fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const createUser = async (req, res = response) => {
   res.json({
@@ -9,13 +8,7 @@ const createUser = async (req, res = response) => {
 };
 
 const login = async (req, res = response) => {
-  const errors = validationResult(req, res);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
+
   const { email, password } = req.body;
 
   res.json({
--- router/auth.js
+++ router/auth.js
@@ -1,16 +1,31 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { createUser, login, renewToken } = require("../controllers/auth");
+const { validateFields } = require("../middlewares/validateFields");
 const router = Router();
 
 // add users
-router.post("/new", createUser);
-
-//login
-router.post("/", [
+router.post(
+  "/new",
+  [
+    check("name", "name is required").not().isEmpty(),
     check("email", "email is required").isEmail(),
     check("password", "password is required").not().isEmpty(),
-], login);
+    validateFields,
+  ],
+  createUser
+);
+
+//login
+router.post(
+  "/",
+  [
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
 
 // revalidate token
 router.get("/renew", renewToken);
