3cd007b7035a4acf6d81b5b3e3711943b9687a32
alan <alanarostegui@hotmail.com>
2021-02-08 12:16:46 -0600

    feature(connect db): connect to mongo atlas

--- models/server.js
+++ models/server.js
@@ -1,51 +1,52 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
-const socketio = require('socket.io');
-const path     = require('path');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const { dbConnection } = require("../database/config");
 
-const Sockets  = require('./sockets');
+const Sockets = require("./sockets");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor() {
+    // Connect db
+    dbConnection();
 
-        this.app  = express();
-        this.port = process.env.PORT;
+    // Http server
+    this.server = http.createServer(this.app);
 
-        // Http server
-        this.server = http.createServer( this.app );
-        
-        // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
-    }
+    // Configuraciones de sockets
+    this.io = socketio(this.server, {
+      /* configuraciones */
+    });
+  }
 
-    middlewares() {
-        // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-    }
+  middlewares() {
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
 
-    // Esta configuración se puede tener aquí o como propieda de clase
-    // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
-    }
+  // Esta configuración se puede tener aquí o como propieda de clase
+  // depende mucho de lo que necesites
+  configurarSockets() {
+    new Sockets(this.io);
+  }
 
-    execute() {
+  execute() {
+    // Inicializar Middlewares
+    this.middlewares();
 
-        // Inicializar Middlewares
-        this.middlewares();
+    // Inicializar sockets
+    this.configurarSockets();
 
-        // Inicializar sockets
-        this.configurarSockets();
-
-        // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
-        });
-    }
-
+    // Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto:", this.port);
+    });
+  }
 }
-
 
 module.exports = Server;
